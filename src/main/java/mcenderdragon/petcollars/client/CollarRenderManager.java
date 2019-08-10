014319909aaadbb94b457437d356c1079d1fb99c
mcenderdragon <goto.minecraft@googlemail.com>
2019-08-10 23:11:36 +0200

    items where rendering upside down, fixed that

--- src/main/java/mcenderdragon/petcollars/client/CollarRenderManager.java
+++ src/main/java/mcenderdragon/petcollars/client/CollarRenderManager.java
@@ -3,6 +3,7 @@ package mcenderdragon.petcollars.client;
 import com.mojang.blaze3d.platform.GlStateManager;
 
 import mcenderdragon.petcollars.client.CollarRenderHelper.ClientCollarState;
+import mcenderdragon.petcollars.common.HelperCollars;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.entity.passive.AnimalEntity;
@@ -23,14 +24,18 @@ public class CollarRenderManager
 	
 	public static void renderCollarAsItem(ClientCollarState collar, AnimalEntity animal, double x, double y, double z, float partialTicks)
 	{
-		y -= animal.getEyeHeight();
+		y += animal.getEyeHeight() * 0.5F;
+		
 		GlStateManager.pushMatrix();
+		
 		GlStateManager.translated(x, y, z);
 		GlStateManager.scaled(1.1, 1.1, 1.1);
 		
 		GlStateManager.rotatef((animal.ticksExisted+partialTicks)*2, 0, 1, 0);
 		
-		ItemStack it = new ItemStack(collar.collarItem);
+		
+		
+		ItemStack it = collar.getAsItem();
 		
 		Minecraft.getInstance().getItemRenderer().renderItem(it, animal, ItemCameraTransforms.TransformType.GROUND, false);
 		
--- src/main/java/mcenderdragon/petcollars/client/rendering/CollarRendererLayer.java
+++ src/main/java/mcenderdragon/petcollars/client/rendering/CollarRendererLayer.java
@@ -1,5 +1,7 @@
 package mcenderdragon.petcollars.client.rendering;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
 import mcenderdragon.petcollars.client.CollarRenderHelper;
 import mcenderdragon.petcollars.client.CollarRenderHelper.ClientCollarState;
 import mcenderdragon.petcollars.client.CollarRenderManager;
@@ -27,7 +29,10 @@ public class CollarRendererLayer extends LayerRenderer<AnimalEntity, EntityModel
 		}
 		else
 		{
+			GlStateManager.pushMatrix();
+			GlStateManager.rotatef(180F, 0, 0, 1);
 			CollarRenderManager.renderCollar(entityIn, st, partialTicks);
+			GlStateManager.popMatrix();
 		}
 		// TODO Auto-generated method stub
 		

cea0efef698abc9bde2825aa51097aadbbb8b44b
MCenderdragon <goto.minecraft@googlemail.com>
2019-08-17 14:06:13 +0200

    new texture for heat resistance pendant

--- src/main/java/mcenderdragon/petcollars/common/pendant/PendantList.java
+++ src/main/java/mcenderdragon/petcollars/common/pendant/PendantList.java
@@ -7,7 +7,7 @@ import mcenderdragon.petcollars.common.PetCollarsMain;
 public class PendantList 
 {
 	public static final PendantBase<?> dummy = new DummyPendant().setRegistryName(PetCollarsMain.MODID, "dummy");
-	public static final PendantBase<?> heat_resistance_pendant = new FireResistancePendant(30, 0xFF6600, 50).setRegistryName(PetCollarsMain.MODID, "heat_resistance_pendant");
+	public static final PendantBase<?> heat_resistance_pendant = new FireResistancePendant(30, 0x4e86d7, 50).setRegistryName(PetCollarsMain.MODID, "heat_resistance_pendant");
 	public static final PendantBase<?> fire_insensitive_pendant = new FireResistancePendant(30, 0xE63B0B, 50).setRegistryName(PetCollarsMain.MODID, "fire_insensitive_pendant");
 	public static final PendantBase<?> fire_resistance_pendant = new FireResistancePendant(20, 0xD12111, 75).setRegistryName(PetCollarsMain.MODID, "fire_resistance_pendant");
 	public static final PendantBase<?> fire_immunity_pendant = new FireResistancePendant(6, 0xFF0000, 125).setRegistryName(PetCollarsMain.MODID, "fire_immunity_pendant");
